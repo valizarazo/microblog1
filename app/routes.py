8e8bb9ec7f76e45f541716ae908a576567a172b2
valizarazo <valizarazo@packer.edu>
2023-02-21 23:33:48 -0500

    Chapters 4 & 5

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,41 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Valentina'}
     posts = [
@@ -16,7 +48,7 @@ def index():
             'body': 'I am feeling tired right now.'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+ return render_template("index.html", title='Home Page', posts=posts)
 @app.route('/songs')
 def songs():
     songs =[
@@ -36,16 +68,29 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
-@app.route('/mystuff')
+@app.route('/account')
 def account():
-    my_songs =[
+    account=[
         {
             'title': 'Mr. Cadillac', 'artist': 'Mac Saturn'
-        }
-        ]
-    my_notes =[
+        },
         {
-            'notes amount': '10'
+            'my notes amount': '10'
         }
         ]
-    return render_template('account.html', title='Account Page', account=account, my_songs=my_songs, my_notes=my_notes)
+
+    return render_template('account.html', title='Account Page', account=account)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
